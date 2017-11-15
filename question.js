fe51a3718a1d7858396d674303bd4aeb692ce8de
Yidan Wang <wangyidan621@gmail.com>
2017-11-15 18:06:41 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,28 @@
 'use strict';
+
+class Question {
+
+
+  static All(){
+    return this._All
+  }
+
+  save(){
+   Question._All.push(this)
+  }
+
+  constructor(content){
+    this.content = content;
+    this.save()
+  }
+
+  static Find(id){
+    return this._All[id-1]
+  }
+
+
+
+
+}
+
+Question._All = []
